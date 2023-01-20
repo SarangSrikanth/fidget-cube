60b76acdf006084663918fcc0770d75a6664caeb
SarangSrikanth <122543645+SarangSrikanth@users.noreply.github.com>
2023-01-20 09:53:28 -0500

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -2,6 +2,51 @@ def on_button_pressed_a():
     basic.show_icon(IconNames.NO)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def on_gesture_tilt_left():
+    basic.show_leds("""
+        # . . # .
+                # . . # .
+                # # # # .
+                # . . # .
+                # . . # .
+    """)
+    basic.show_leds("""
+        # # # # .
+                # . . . .
+                # # # # .
+                # . . . .
+                # # # # .
+    """)
+    basic.show_leds("""
+        # . . . .
+                # . . . .
+                # . . . .
+                # . . . .
+                # . . . .
+    """)
+    basic.show_leds("""
+        # # # . .
+                # . . # .
+                # # # . .
+                # . . . .
+                # . . . .
+    """)
+    basic.show_leds("""
+        # . . . .
+                # . . . .
+                # . . . .
+                . . . . .
+                # . . . .
+    """)
+    basic.show_leds("""
+        . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+    """)
+input.on_gesture(Gesture.TILT_LEFT, on_gesture_tilt_left)
+
 def on_button_pressed_ab():
     basic.show_icon(IconNames.SMALL_HEART)
     basic.show_icon(IconNames.HEART)
@@ -14,6 +59,18 @@ input.on_button_pressed(Button.B, on_button_pressed_b)
 def on_gesture_shake():
     music.start_melody(music.built_in_melody(Melodies.RINGTONE), MelodyOptions.ONCE)
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+def on_gesture_tilt_right():
+    music.play_sound_effect(music.create_sound_effect(WaveShape.SINE,
+            4882,
+            0,
+            255,
+            0,
+            500,
+            SoundExpressionEffect.NONE,
+            InterpolationCurve.LINEAR),
+        SoundExpressionPlayMode.UNTIL_DONE)
+input.on_gesture(Gesture.TILT_RIGHT, on_gesture_tilt_right)
 
 def on_logo_touched():
     basic.show_leds("""
--- main.ts
+++ main.ts
@@ -1,25 +1,72 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     basic.showIcon(IconNames.No)
 })
-input.onButtonPressed(Button.AB, function () {
+input.onGesture(Gesture.TiltLeft, function on_gesture_tilt_left() {
+    basic.showLeds(`
+        # . . # .
+                # . . # .
+                # # # # .
+                # . . # .
+                # . . # .
+    `)
+    basic.showLeds(`
+        # # # # .
+                # . . . .
+                # # # # .
+                # . . . .
+                # # # # .
+    `)
+    basic.showLeds(`
+        # . . . .
+                # . . . .
+                # . . . .
+                # . . . .
+                # . . . .
+    `)
+    basic.showLeds(`
+        # # # . .
+                # . . # .
+                # # # . .
+                # . . . .
+                # . . . .
+    `)
+    basic.showLeds(`
+        # . . . .
+                # . . . .
+                # . . . .
+                . . . . .
+                # . . . .
+    `)
+    basic.showLeds(`
+        . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+    `)
+})
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
     basic.showIcon(IconNames.SmallHeart)
     basic.showIcon(IconNames.Heart)
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     basic.showIcon(IconNames.Yes)
 })
-input.onGesture(Gesture.Shake, function () {
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
     music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once)
 })
-input.onLogoEvent(TouchButtonEvent.Touched, function () {
+input.onGesture(Gesture.TiltRight, function on_gesture_tilt_right() {
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 4882, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function on_logo_touched() {
     basic.showLeds(`
         . . # . .
-        . . # . .
-        . . # . .
-        . . . . .
-        . . # . .
-        `)
+                . . # . .
+                . . # . .
+                . . . . .
+                . . # . .
+    `)
 })
-basic.forever(function () {
-	
+basic.forever(function on_forever() {
+    
 })
