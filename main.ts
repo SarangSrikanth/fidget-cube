9b9abf8b49f7106130c83e4881054912b263f4b5
SarangSrikanth <122543645+SarangSrikanth@users.noreply.github.com>
2023-01-20 09:53:54 -0500

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,72 +1,72 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
+input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.No)
 })
-input.onGesture(Gesture.TiltLeft, function on_gesture_tilt_left() {
+input.onGesture(Gesture.TiltLeft, function () {
     basic.showLeds(`
         # . . # .
-                # . . # .
-                # # # # .
-                # . . # .
-                # . . # .
-    `)
+        # . . # .
+        # # # # .
+        # . . # .
+        # . . # .
+        `)
     basic.showLeds(`
         # # # # .
-                # . . . .
-                # # # # .
-                # . . . .
-                # # # # .
-    `)
+        # . . . .
+        # # # # .
+        # . . . .
+        # # # # .
+        `)
     basic.showLeds(`
         # . . . .
-                # . . . .
-                # . . . .
-                # . . . .
-                # . . . .
-    `)
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        `)
     basic.showLeds(`
         # # # . .
-                # . . # .
-                # # # . .
-                # . . . .
-                # . . . .
-    `)
+        # . . # .
+        # # # . .
+        # . . . .
+        # . . . .
+        `)
     basic.showLeds(`
         # . . . .
-                # . . . .
-                # . . . .
-                . . . . .
-                # . . . .
-    `)
+        # . . . .
+        # . . . .
+        . . . . .
+        # . . . .
+        `)
     basic.showLeds(`
         . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-    `)
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 })
-input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+input.onButtonPressed(Button.AB, function () {
     basic.showIcon(IconNames.SmallHeart)
     basic.showIcon(IconNames.Heart)
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
+input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Yes)
 })
-input.onGesture(Gesture.Shake, function on_gesture_shake() {
+input.onGesture(Gesture.Shake, function () {
     music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once)
 })
-input.onGesture(Gesture.TiltRight, function on_gesture_tilt_right() {
+input.onGesture(Gesture.TiltRight, function () {
     music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 4882, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
 })
-input.onLogoEvent(TouchButtonEvent.Touched, function on_logo_touched() {
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
     basic.showLeds(`
         . . # . .
-                . . # . .
-                . . # . .
-                . . . . .
-                . . # . .
-    `)
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        `)
 })
-basic.forever(function on_forever() {
-    
+basic.forever(function () {
+	
 })
